1837cdaf592936adb545d3cf9a4ec26bb3b3e797
KondoSuguru <suguru.pkmn@gmail.com>
2019-09-11 15:30:59 +0900

    Actor、ActorManager追加

---
+++ LoopGame/LoopGame/Actor/ActorManager.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopGame.Actor
+{
+    sealed class ActorManager
+    {
+        private static ActorManager mInstance;
+        private List<Actor> mActors;
+
+        private ActorManager()
+        {
+            mActors = new List<Actor>();
+        }
+
+        public static ActorManager Instance()
+        {
+            if(mInstance == null)
+            {
+                mInstance = new ActorManager();
+            }
+            return mInstance;
+        }
+
+        public void Initialize()
+        {
+            if(mActors != null)
+            {
+                mActors.Clear();
+            }
+        }
+
+        public void AddActor(Actor actor)
+        {
+            if (actor == null)
+                return;
+            mActors.Add(actor);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach(var a in mActors)
+            {
+                a.Update(gameTime);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (var a in mActors)
+            {
+                a.Draw();
+            }
+        }
+
+        public void Clear()
+        {
+            mActors.Clear();
+        }
+    }
+}
