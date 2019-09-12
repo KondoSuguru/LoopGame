d32547237729a0416e87e6b962a1debc48537cfc
KondoSuguru <suguru.pkmn@gmail.com>
2019-09-12 11:59:11 +0900

    Space、Goal追加

--- LoopGame/LoopGame/Actor/ActorMove.cs
+++ LoopGame/LoopGame/Actor/ActorMove.cs
@@ -18,21 +18,37 @@ namespace LoopGame.Actor
         public void MoveLeft(ref Vector2 pos)
         {          
             pos.X -= GridSize.GRID_SIZE;
+            if(pos.X <= -GridSize.GRID_SIZE)
+            {
+                pos.X = Screen.WIDTH - GridSize.GRID_SIZE;
+            }
         }
 
         public void MoveRight(ref Vector2 pos)
         {
             pos.X += GridSize.GRID_SIZE;
+            if (pos.X >= Screen.WIDTH)
+            {
+                pos.X = 0;
+            }
         }
 
         public void MoveUp(ref Vector2 pos)
         {
             pos.Y -= GridSize.GRID_SIZE;
+            if(pos.Y <= -GridSize.GRID_SIZE)
+            {
+                pos.Y = Screen.HEIGHT - GridSize.GRID_SIZE;
+            }
         }
 
         public void MoveDown(ref Vector2 pos)
         {
             pos.Y += GridSize.GRID_SIZE;
+            if (pos.Y >= Screen.HEIGHT)
+            {
+                pos.Y = 0;
+            }
         }
     }
 }
